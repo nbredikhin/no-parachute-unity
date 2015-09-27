4ffec9e243c89bfa35c391cee2db07c05b944ab2
Nikita Bredikhin <bredikhin.nikita@gmail.com>
2015-09-27 17:55:34 +0300

    Добавлен уровень 10

--- assets/levels/10/logic.lua
+++ assets/levels/10/logic.lua
@@ -5,12 +5,14 @@ local LevelLogicBase = require "LevelLogic"
 local LevelLogic = Core.class(LevelLogicBase)
 
 function LevelLogic:init()
-	self.requiredTime = 120
+	self.requiredTime = 150
 	self.planesCount = 2
 end
 
 function LevelLogic:initialize()
-	self:setFallingSpeed(self.defaultFallingSpeed * 1.2)
+	self:setCameraType(LevelLogic.CAMERA_ROTATING_SIN)
+	self:setCameraSpeed(90)
+	self:setFallingSpeed(self.defaultFallingSpeed * 1.6)
 end
 
 return LevelLogic
