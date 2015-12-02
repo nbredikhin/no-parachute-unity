03f1a5ef97d2c9098eaade92e65ddca3834ac505
Nikita Bredikhin <bredikhin.nikita@gmail.com>
2015-12-03 00:43:48 +0300

    Фон меню, экран паузы
    - Исправленный фон меню
    - Экран паузы
    - Переход между экранами в игре

--- Assets/scripts/menu/MenuBackground.cs
+++ Assets/scripts/menu/MenuBackground.cs
@@ -4,7 +4,25 @@ using System.Collections;
 
 public class MenuBackground : MonoBehaviour
 {
+    private Image image;
+    private Texture2D texture;
+
     void Start()
     {
+        image = GetComponent<Image>();
+        texture = new Texture2D(100, 1);
+        texture.filterMode = FilterMode.Point;
+        image.sprite = Sprite.Create(texture, new Rect(0, 0, 100, 1), Vector2.zero);
     }
+
+    void Update()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            var mul = (float)i / 100f * 0.2f;
+            texture.SetPixel(i, 0, new Color(Random.value * mul, Random.value * mul, Random.value * mul));
+        }
+        texture.Apply();
+    }
+
 }
