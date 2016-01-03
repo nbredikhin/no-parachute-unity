f2f096c275f87b55000f6b10c641d659fbbad523
Nikita Bredikhin <bredikhin.nikita@gmail.com>
2016-01-04 01:47:59 +0300

    Вращение камеры, кнопка Restart level, zlib
    - Добавлена кнопка "Restart level" в меню паузы
    - Отключена библиотека SharpZipLib на Windows Phone
    - Добавлены типы вращения камеры

---
+++ Assets/scripts/levels/camera_rotation_behavours/FollowingPlayerRotation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowingPlayerRotation : BaseCameraRotationScript 
+{
+    private float cameraSpeed;
+    void Start()
+    {
+        cameraSpeed = 0;
+    }
+    void Update()
+    {
+        cameraSpeed -= JoystickInput.input.x * Time.deltaTime * RotationSpeed;
+        cameraSpeed *= 0.99f;
+        transform.Rotate(0f, 0f, cameraSpeed);
+    }
+    
+    public override void Setup(float RotationSpeed, params Object [] AdditionalArguments)
+    {
+        base.Setup(RotationSpeed, AdditionalArguments);
+    } 
+}
---
+++ Assets/scripts/levels/camera_rotation_behavours/SineCameraRotation.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SineCameraRotation : BaseCameraRotationScript 
+{
+    void Update()
+    {
+        transform.Rotate(0f, 0f, RotationSpeed * Mathf.Sin(Time.time) * Time.deltaTime);
+    }
+    
+    public override void Setup(float RotationSpeed, params Object [] AdditionalArguments)
+    {
+        base.Setup(RotationSpeed, AdditionalArguments);
+    } 
+}
