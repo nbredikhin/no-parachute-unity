b6d656854632e0fb6f1ffea43a091407db85c4c0
Nikita Bredikhin <zhe3145@yandex.ru>
2016-01-12 19:17:33 +0300

    Базовая локализация
    - Локализация всех сцен
    - Музыка
    - Основа внутриигровых покупок

---
+++ Assets/scripts/ui/Localizer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Localizer : MonoBehaviour 
+{
+    public StringType type;
+    
+	void Start () 
+    {
+	   var text = GetComponent<Text>();
+       // Если мы повесили компонент на кнопку
+       if (text == null)
+       {
+           text = GetComponentInChildren<Text>();
+       }
+       text.text = LocalizedStrings.GetString(type);
+	}
+}
---
+++ Assets/scripts/utils/LocalizedStrings.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+public enum StringType
+{
+    StartGame,
+    EndlessMode,
+    EndlessModeUnlockText,
+    Settings,
+    Sound,
+    Vibration,
+    Quality,
+    Sensitivity,
+    StateEnabled,
+    StateDisabled,
+    StateLow,
+    StateMedium,
+    StateHigh,
+    Credits,
+    CreditsText,
+    Back,
+    Start,
+    Continue,
+    RestartLevel,
+    BackToMenu,
+    TryAgain
+}
+
+public static class LocalizedStrings
+{
+    private static SystemLanguage language = Application.systemLanguage;
+
+    private static string[,] localizedStrings =
+    {{"Start game", "Начать игру"},
+     {"Endless mode", "Бесконечный режим"},
+     {"Complete all levels to unlock", "Пройдите все уровни для открытия"},
+     {"Settings", "Настройки"},
+     {"Sound", "Звуки"},
+     {"Vibration", "Вибрация"},
+     {"Quality", "Качество"},
+     {"Sensitivity:", "Чувствительность:"},
+     {"enabled", "вкл."},
+     {"disabled", "выкл."},
+     {"low", "низкое"},
+     {"medium", "среднее"},
+     {"high", "высокое"},
+     {"Credits", "Об авторах"},
+     {"No Parachute!\n\nProgramming, design:\nNikita Bredikhin\n\nProgramming, music:\nEugene Morozov", "No Parachute!\n\nПрограммирование, дизайн:\nНикита Бредихин\n\nПрограммирование, музыка:\nЕвгений Морозов"},
+     {"Back", "Назад"},
+     {"START", "НАЧАТЬ"},
+     {"Continue", "Продолжить"},
+     {"Restart level", "Начать сначала"},
+     {"Back to menu", "Вернуться в меню"},
+     {"Try again", "Попробовать снова"}
+     };
+
+    public static string GetString(StringType title)
+    {
+        int lang = -1;
+        if (language == SystemLanguage.English)
+            lang = 0;
+        else if (language == SystemLanguage.Russian)
+            lang = 1;
+        return localizedStrings[(uint)title, lang];
+    }
+}
