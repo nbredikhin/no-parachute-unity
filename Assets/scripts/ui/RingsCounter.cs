8f612311664b18d1039640a1fdbf6ddc9f337744
Nikita Bredikhin <bredikhin.nikita@gmail.com>
2016-01-31 04:40:26 +0300

    Отображение количества колец и стоимости персонажа

---
+++ Assets/scripts/ui/RingsCounter.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class RingsCounter : MonoBehaviour {
+    public Text counterText;
+	void Start () {
+	   
+	}
+    
+	void Update () {
+	   counterText.text = CoinsManager.Balance.ToString();
+	}
+}
